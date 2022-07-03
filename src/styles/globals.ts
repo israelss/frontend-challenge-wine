6d21a3c292affb682ed00aaba08e8f974751c154
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 14:08:03 -0300

    style: create global styles

---
+++ src/styles/globals.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Lato', sans-serif;
+  }
+  a {
+    color: ${({ theme }) => theme.colors.gray};
+    text-decoration: none;
+  }
+`
+
+export default GlobalStyles
