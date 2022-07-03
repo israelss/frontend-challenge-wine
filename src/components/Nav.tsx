e885b15f228115cafbb479382057ba0be05e47d1
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 12:33:27 -0300

    feat(components): create `Nav`

---
+++ src/components/Nav.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <div>
+      <Link href="/clube">
+        Clube
+      </Link>
+      <Link href="/loja">
+        Loja
+      </Link>
+      <Link href="/produtores">
+        Produtores
+      </Link>
+      <Link href="/ofertas">
+        Ofertas
+      </Link>
+      <Link href="/eventos">
+        Eventos
+      </Link>
+    </div>
+  );
+};
+
+export default Nav
