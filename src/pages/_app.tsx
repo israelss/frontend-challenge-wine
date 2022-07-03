50b65e79f80cb78178112ddf2fdc522676444f6a
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 12:45:46 -0300

    feat(_app): add `FilterProvider` on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import BaseLayout from '@components/BaseLayout'
 import { CartProvider } from '@contexts/cart'
+import { FilterProvider } from '@contexts/filter'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CartProvider>
-      <BaseLayout>
-        <Component {...pageProps} />
-      </BaseLayout>
-    </CartProvider>
+    <FilterProvider>
+      <CartProvider>
+        <BaseLayout>
+          <Component {...pageProps} />
+        </BaseLayout>
+      </CartProvider>
+    </FilterProvider>
   )
 }
 
