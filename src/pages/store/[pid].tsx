2f851a3e3dca364536077c7503a6acde52e42e99
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 13:23:11 -0300

    feat(pages): create `store/[pid]`

---
+++ src/pages/store/[pid].tsx
@@ -0,0 +1,71 @@
+import { GetStaticProps, GetStaticPaths, GetStaticPropsContext } from "next"
+import { Product, ProductsApiReturn } from "src/types/product";
+import { useRouter } from 'next/router'
+import ProductDetails from "@components/ProductDetails";
+
+const requestOptions: RequestInit = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+type ProductDetails = {
+  product: Product;
+}
+
+const Details = ({ product }: ProductDetails) => {
+  const router = useRouter()
+
+  if (router.isFallback) return <h1>Carregando detalhes do produto...</h1>
+
+  return (
+    <div>
+      <ProductDetails product={ product } />
+    </div>
+    )
+  }
+
+  export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+      paths: [
+        { params: { pid: '0' } },
+        { params: { pid: '1' } },
+        { params: { pid: '2' } },
+        { params: { pid: '3' } },
+        { params: { pid: '4' } },
+        { params: { pid: '5' } },
+        { params: { pid: '6' } },
+        { params: { pid: '7' } },
+        { params: { pid: '8' } },
+        { params: { pid: '9' } },
+      ],
+      fallback: true
+    };
+  }
+
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const { pid } = params!
+
+  if (!pid) return { props: {} }
+
+  const response = await fetch('https://wine-back-test.herokuapp.com/products', requestOptions)
+  const products: ProductsApiReturn = await response.json()
+  const product = products.items.find(({ id }: Product) => id === +pid)
+
+  if (!product) {
+    return {
+      redirect: {
+        destination: '/loja',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 30
+  }
+}
+
+export default Details
