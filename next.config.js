2c4e16e29d7eba5429dae49d1296fb624f357f36
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 13:28:14 -0300

    build(next.config): configure `rewrites`
    - add rewrite from `/conta` to `/account`
    - add rewrite from `/clube` to `/club`
    - add rewrite from `/loja/vinho/:pid` to `/store/:pid`
    - add rewrite from `/produtores` to `/makers`
    - add rewrite from `/ofertas` to `/offers`
    - add rewrite from `/eventos` to `/events`

--- next.config.js
+++ next.config.js
@@ -4,12 +4,36 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/conta',
+        destination: '/account'
+      },
+      {
+        source: '/clube',
+        destination: '/club'
+      },
+      {
         source: '/loja/mobile',
         destination: '/store/mobile'
       },
       {
         source: '/loja',
         destination: '/store'
+      },
+      {
+        source: '/loja/vinho/:pid',
+        destination: '/store/:pid'
+      },
+      {
+        source: '/produtores',
+        destination: '/makers'
+      },
+      {
+        source: '/ofertas',
+        destination: '/offers'
+      },
+      {
+        source: '/eventos',
+        destination: '/events'
       },
     ];
   }
