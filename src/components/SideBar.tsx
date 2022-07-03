0bc825972f44395fd5a6bcf5826f6aebf4e2554a
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 12:38:54 -0300

    feat(components): create `SideBar`

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import UserAccount from "@components/UserAccount"
+import WineIcon from "@components/WineIcon"
+import Nav from "./Nav"
+
+const SideBar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>
+        <WineIcon id='menu-icon' width={32} height={32} fill="#111111" />
+      </button>
+      {
+        isOpen && (
+          <div>
+            <button onClick={() => setIsOpen(false)}>
+              <WineIcon id='close-icon' width={40} height={40} fill="#1D1D1D" />
+            </button>
+            <UserAccount />
+            <Nav />
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default SideBar
