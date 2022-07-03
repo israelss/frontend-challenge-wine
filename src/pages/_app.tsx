afcbefae89f8c138c13e30819fdbb62cfda4151e
Israel Sant'Anna <israel.santanna@gmail.com>
2022-07-03 12:45:18 -0300

    feat(_app): add `CartProvider` on all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import BaseLayout from '@components/BaseLayout'
+import { CartProvider } from '@contexts/cart'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <BaseLayout>
-      <Component {...pageProps} />
-    </BaseLayout>
+    <CartProvider>
+      <BaseLayout>
+        <Component {...pageProps} />
+      </BaseLayout>
+    </CartProvider>
   )
 }
 
